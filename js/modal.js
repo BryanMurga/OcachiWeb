a6ee11ed178748a5562217eabd7c54a5d531dc20
Fabian <112954@alumnouninter.mx>
2026-05-12 12:14:02 -0600

    Padres de Familia

--- js/modal.js
+++ js/modal.js
@@ -177,7 +177,7 @@ import {
       creadoEn: serverTimestamp(),
     };
 
-    addDoc(collection(db, "eventoOpenSchool"), registro)
+    addDoc(collection(db, "eventoTeatroOcachi"), registro)
       .then(function () {
         qs("ocachiFormSection").style.display = "none";
         qs("ocachiSuccess").classList.add("show");
